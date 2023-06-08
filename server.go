084820a9d50359caf972f2d6660365bd58f622b9
Luis Guerreiro <luiscvlh11@gmail.com>
2023-06-08 00:13:47 -0300

    feat[modulo]: Objetos de configuração

--- server.go
+++ server.go
@@ -1,12 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"fmt";
+	"io/ioutil";
+	"log";
+	"net/http";
+	"os";
+)
+
 
 func main() {
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello %s, are you %s?", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatal("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "My Family: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s, Password: %s", user, password)
 }
