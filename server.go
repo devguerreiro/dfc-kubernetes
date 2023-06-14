e162b99cce87ec60a37bf0c0e95a31e80ee876f7
Luis Guerreiro <luiscvlh11@gmail.com>
2023-06-14 19:55:35 -0300

    feat[modulo]: probes

--- server.go
+++ server.go
@@ -6,10 +6,13 @@ import (
 	"log";
 	"net/http";
 	"os";
+	"time";
 )
 
+var startedAt = time.Now()
 
 func main() {
+	http.HandleFunc("/health-check", HealthCheck)
 	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
@@ -34,4 +37,16 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
 	fmt.Fprintf(w, "User: %s, Password: %s", user, password)
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
